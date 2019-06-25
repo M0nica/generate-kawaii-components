112f15dcd74cb681a8e44a222e6a09a36ec66777
Monica Powell <monica@aboutmonica.com>
2019-06-25 18:37:48 -0400

    customize meta data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import ExcitedCat from './ExcitedCat';
+import ExcitedCreditCard from './ExcitedCreditCard';
 
 ReactDOM.render(
   <div>
     <h1>Demo</h1>
+<ExcitedCat />
+<ExcitedCreditCard />
   </div>,
 
   document.getElementById("root")
