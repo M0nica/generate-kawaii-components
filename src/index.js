987c0eb5eda73830d4f8242ce68198122febe31d
Monica Powell <monica@aboutmonica.com>
2019-06-24 22:49:05 -0400

    remove unecessary comma and demo examples

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import LovestruckGhost from './LovestruckGhost';
 
 ReactDOM.render(
   <div>
     <h1>Demo</h1>
-<LovestruckGhost />,
   </div>,
 
   document.getElementById("root")
