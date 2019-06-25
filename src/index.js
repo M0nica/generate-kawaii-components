b402281a9d657236744ffe0ad8354e102ac409ca
Monica Powell <monica@aboutmonica.com>
2019-06-25 18:38:55 -0400

    remove old generated components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import ExcitedCat from './ExcitedCat';
-import ExcitedCreditCard from './ExcitedCreditCard';
+
 
 ReactDOM.render(
   <div>
     <h1>Demo</h1>
-<ExcitedCat />
-<ExcitedCreditCard />
   </div>,
 
   document.getElementById("root")
